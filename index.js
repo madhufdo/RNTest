6936576230032d18c97026f77943594bcdc380b9
Madhumathy Fernando <Fernando@Madhumathys-Air.BigPond>
2018-07-13 19:52:44 +1000

    - Initialized react native - Added Header component

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+//import App from './App';
+//import { Text } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+//<Header headerText = {'Albums'}/>
+<Header/>
+ );
+
+
+AppRegistry.registerComponent('RNTest', () => App);
